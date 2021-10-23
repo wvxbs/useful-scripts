7fdcd8d488ed4880caac69a2504955b399c65a23
wvxbs <gabriel.ferreira7854@gmail.com>
2021-10-22 22:22:14 -0300

    changes made to NewMachine.sh

--- NewMachine.sh
+++ NewMachine.sh
@@ -4,43 +4,43 @@ EMAIL=""
 
 sudo $PM update
 
-sudo $PM install firefox 
+sudo $PM install firefox -y
 
-sudo $PM install geary
+sudo $PM install geary -y
 
-sudo $PM install git 
+sudo $PM install git  -y
 
 git config --global user.name $USER_NAME
 git config --global user.email $EMAIL
 
-sudo $PM install python
+sudo $PM install python -y
 
-sudo $PM install pyhton3
+sudo $PM install pyhton3 -y
 
-sudo $PM install pip
+sudo $PM install pip -y
 
-sudo pip install undervolt
+sudo pip install undervolt -y
 
-sudo $PM install tlp
+sudo $PM install tlp -y
 
-sudo $PM install obs-studio
+sudo $PM install obs-studio -y
 
-sudo $PM install gnome-pomodoro
+sudo $PM install gnome-pomodoro -y
 
-sudo $PM install flatkpak
+sudo $PM install flatkpak -y
 
-sudo flatpak install flathub com.spotify.Client
+sudo flatpak install flathub com.spotify.Client -y
 
-sudo flatpak install flathub com.discordapp.Discord
+sudo flatpak install flathub com.discordapp.Discord -y
 
-sudo flatpak install flathub us.zoom.Zoom
+sudo flatpak install flathub us.zoom.Zoom -y
 
-sudo flatpak install flathub com.visualstudio.code
+sudo flatpak install flathub com.visualstudio.code -y
 
-sudo flatpak install flathub org.chromium.Chromium
+sudo flatpak install flathub org.chromium.Chromium -y 
 
-sudo flatpak install flathub com.github.alainm23.planner
+sudo flatpak install flathub com.github.alainm23.planner -y
 
-sudo flatpak install flathub com.google.AndroidStudio
+sudo flatpak install flathub com.google.AndroidStudio -y
 
-sudo flatpak install flathub com.getpostman.Postman
+sudo flatpak install flathub com.getpostman.Postman -y
