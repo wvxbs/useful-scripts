7edaf8a6e4010c4de197d138e0db8aed1c902d4a
wvxbs <gabriel.ferreira7854@gmail.com>
2021-11-01 14:40:38 -0300

    updates to NewMachine.sh

--- NewMachine.sh
+++ NewMachine.sh
@@ -10,8 +10,12 @@ sudo $PM install geary -y
 
 sudo $PM install git  -y
 
+sudo $PM install neofetch -y
+
 git config --global user.name $USER_NAME
 git config --global user.email $EMAIL
+
+sudo $PM install npm -y
 
 sudo $PM install python -y
 
@@ -29,18 +33,18 @@ sudo $PM install gnome-pomodoro -y
 
 sudo $PM install flatpak -y
 
-sudo flatpak install flathub com.spotify.Client -y
+sudo flatpak install flathub com.spotify.Client
 
-sudo flatpak install flathub com.discordapp.Discord -y
+sudo flatpak install flathub com.discordapp.Discord
 
-sudo flatpak install flathub us.zoom.Zoom -y
+sudo flatpak install flathub us.zoom.Zoom
 
-sudo flatpak install flathub com.visualstudio.code -y
+sudo flatpak install flathub com.visualstudio.code
 
-sudo flatpak install flathub org.chromium.Chromium -y 
+sudo flatpak install flathub org.chromium.Chromium
 
-sudo flatpak install flathub com.github.alainm23.planner -y
+sudo flatpak install flathub com.github.alainm23.planner
 
-sudo flatpak install flathub com.google.AndroidStudio -y
+sudo flatpak install flathub com.google.AndroidStudio
 
-sudo flatpak install flathub com.getpostman.Postman -y
+sudo flatpak install flathub com.getpostman.Postman
