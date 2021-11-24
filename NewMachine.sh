c7baa7c29360b25a3df712333f73c29e3f63458e
Gabriel Ferreira <gabriel.ferreira7854@gmail.com>
2021-11-23 22:16:14 -0300

    Update NewMachine.sh

--- NewMachine.sh
+++ NewMachine.sh
@@ -2,7 +2,9 @@ PM=
 USER_NAME=""
 EMAIL=""
 
-sudo $PM update
+sudo $PM update -y
+
+sudo $PM upgrade -y
 
 sudo $PM install firefox -y
 
@@ -25,28 +27,20 @@ sudo $PM install pip -y
 
 sudo pip install undervolt -y
 
-sudo $PM install tlp -y
-
-sudo $PM install obs-studio -y
-
 sudo $PM install gnome-pomodoro -y
 
 sudo $PM install flatpak -y
 
-sudo flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
+sudo flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo -y
 
-sudo flatpak install flathub com.spotify.Client
+sudo flatpak install flathub com.spotify.Client -y
 
-sudo flatpak install flathub com.discordapp.Discord
+sudo flatpak install flathub com.discordapp.Discord -y
 
-sudo flatpak install flathub us.zoom.Zoom
+sudo flatpak install flathub us.zoom.Zoom -y 
 
-sudo flatpak install flathub com.visualstudio.code
+sudo flatpak install flathub org.chromium.Chromium -y 
 
-sudo flatpak install flathub org.chromium.Chromium
+sudo flatpak install flathub com.google.AndroidStudio -y
 
-sudo flatpak install flathub com.github.alainm23.planner
-
-sudo flatpak install flathub com.google.AndroidStudio
-
-sudo flatpak install flathub com.getpostman.Postman
+sudo flatpak install flathub com.getpostman.Postman -y
