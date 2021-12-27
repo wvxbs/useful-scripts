40675ba497036500a5479734c3360e1bbd26fbfa
wvxbs <gabriel.ferreira7854@gmail.com>
2021-12-27 14:31:03 -0300

    changes made to newmachine.sh

--- NewMachine.sh
+++ NewMachine.sh
@@ -6,38 +6,19 @@ sudo $PM update -y
 
 sudo $PM upgrade -y
 
-sudo $PM install firefox -y
-
-sudo $PM install geary -y
-
-sudo $PM install git  -y
-
-sudo $PM install neofetch -y
-
-sudo $PM install node nodejs -y
-
-git config --global user.name $USER_NAME
-git config --global user.email $EMAIL
-
-sudo $PM install python pyhton3 pip -y
+sudo $PM install -y firefox geary git neofetch -y node nodejs python pyhton3 pip gnome-pomodoro dnf-plugins-core flatpak
 
 sudo pip install undervolt
 
-sudo $PM install gnome-pomodoro -y
-
 sudo $PM remove -y docker docker-client docker-client-latest docker-common docker-latest docker-latest-logrotate docker-logrotate docker-selinux docker-engine-selinux docker-engine
-
-sudo $PM install dnf-plugins-core -y
 
 sudo $PM config-manager --add-repo https://download.docker.com/linux/fedora/docker-ce.repo
 
-sudo $PM install docker-ce docker-ce-cli containerd.io -y 
+sudo $PM install docker-ce docker-ce-cli containerd.io -y
 
-sudo $PM install flatpak -y
+sudo flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
 
-sudo flatpak remote-add flathub https://flathub.org/repo/flathub.flatpakrepo
-
-sudo flatpak install flathub com.spotify.Client
+sudo flatpak run com.spotify.Client
 
 sudo flatpak install flathub com.discordapp.Discord
 
@@ -48,3 +29,7 @@ sudo flatpak install flathub org.chromium.Chromium
 sudo flatpak install flathub com.google.AndroidStudio
 
 sudo flatpak install flathub com.getpostman.Postman
+
+git config --global user.name $USER_NAME
+
+git config --global user.email $EMAIL
