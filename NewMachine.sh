86e63f1100b9609b0e0c07b6cc904564036dfa05
wvxbs <gabriel.ferreira7854@gmail.com>
2022-01-09 18:55:11 -0300

    changes made to NewMachine.sh

--- NewMachine.sh
+++ NewMachine.sh
@@ -1,22 +1,17 @@
 PM=
 USER_NAME=
 EMAIL=
-TOUCHEGG=
-
 sudo $PM update
 sudo $PM upgrade -y
 
-sudo $PM install -y firefox geary git neofetch node nodejs python pyhton3 pip gnome-pomodoro dnf-plugins-core flatpak make akmod-nvidia xorg-x11-drv-nvidia-cuda
-sudo pip install undervolt
+sudo $PM install -y firefox geary git neofetch gnome-pomodoro dnf-plugins-core flatpak make akmod-nvidia xorg-x11-drv-nvidia-cuda
 
 sudo $PM remove -y docker docker-client docker-client-latest docker-common docker-latest docker-latest-logrotate docker-logrotate docker-selinux docker-engine-selinux docker-engine
 sudo $PM config-manager --add-repo https://download.docker.com/linux/fedora/docker-ce.repo
-sudo $PM install -y docker-ce docker-ce-cli containerd.io
-
-$TOUCHEGG
-sudo $PM install -y touchegg
+sudo $PM install -y docker-ce docker-ce-cli containerd.io docker-compose
 
 sudo flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
+sudo flatpak remote-add --if-not-exists --system appcenter https://flatpak.elementary.io/repo.flatpakrepo
 
 sudo flatpak install flathub com.spotify.Client
 sudo flatpak install flathub com.obsproject.Studio 
@@ -30,5 +25,3 @@ sudo flatpak install flathub fr.handbrake.ghb
 
 git config --global user.name $USER_NAME
 git config --global user.email $EMAIL
-
-sudo reboot
