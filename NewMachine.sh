6d0ddbabae174e071543aca270c4a97c997617c1
wvxbs <gabriel.ferreira7854@gmail.com>
2022-01-22 14:55:29 -0300

    code cleanup

--- NewMachine.sh
+++ NewMachine.sh
@@ -1,27 +1,33 @@
 PM=
 USER_NAME=
 EMAIL=
-sudo $PM update
+
+sudo $PM update -y
+
 sudo $PM upgrade -y
 
-sudo $PM install -y firefox geary git neofetch gnome-pomodoro dnf-plugins-core flatpak make akmod-nvidia xorg-x11-drv-nvidia-cuda
+sudo $PM install -y firefox geary git neofetch nodejs python pyhton3 gnome-pomodoro dnf-plugins-core flatpak
 
 sudo $PM remove -y docker docker-client docker-client-latest docker-common docker-latest docker-latest-logrotate docker-logrotate docker-selinux docker-engine-selinux docker-engine
+
 sudo $PM config-manager --add-repo https://download.docker.com/linux/fedora/docker-ce.repo
-sudo $PM install -y docker-ce docker-ce-cli containerd.io docker-compose
+
+sudo $PM install docker-ce docker-ce-cli containerd.io -y
 
 sudo flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
-sudo flatpak remote-add --if-not-exists --system appcenter https://flatpak.elementary.io/repo.flatpakrepo
 
-sudo flatpak install flathub com.spotify.Client
-sudo flatpak install flathub com.obsproject.Studio 
-sudo flatpak install flathub org.darktable.Darktable
-sudo flatpak install flathub org.gimp.GIMP
+sudo flatpak install com.spotify.Client
+
 sudo flatpak install flathub com.discordapp.Discord
+
 sudo flatpak install flathub us.zoom.Zoom
+
+sudo flatpak install flathub org.chromium.Chromium
+
 sudo flatpak install flathub com.google.AndroidStudio
+
 sudo flatpak install flathub com.getpostman.Postman
-sudo flatpak install flathub fr.handbrake.ghb
 
 git config --global user.name $USER_NAME
+
 git config --global user.email $EMAIL
